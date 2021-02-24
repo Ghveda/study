84d8a764fbbcc76421951f9d7f9509f54af554e3
Ghveda <ghvedashvili1997@gmail.com>
2021-02-24 22:30:38 +0400

    commited

--- tryone/blog/views.py
+++ tryone/blog/views.py
@@ -1,26 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import post
 
 # Create your views here.
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
-    }
-]
+
 
 def home(request):
-    context = {'post': posts}
+    context = {'post': post.objects.all()}
     return render(request, 'blog/home.html', context)
 
 def about(request):
-    context = {'post': posts}
-    return render(request, 'blog/about.html', context)
+    return render(request, 'blog/about.html')
